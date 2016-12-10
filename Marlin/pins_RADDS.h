9365dbd5dbb6ad89dc234e7996ada8f600b9affb
esenapaj <creation0004@gmail.com>
2016-12-11 05:14:18 +0900

    Change default MAX6675_SS pin of RADDS to D5

--- Marlin/pins_RADDS.h
+++ Marlin/pins_RADDS.h
@@ -90,9 +90,9 @@
 
 // SPI for Max6675 or Max31855 Thermocouple
 #if DISABLED(SDSUPPORT)
-  #define MAX6675_SS        53
+  #define MAX6675_SS        5 // shared with SERVO0_PIN
 #else
-  #define MAX6675_SS        49
+  #define MAX6675_SS        5 // shared with SERVO0_PIN
 #endif
 
 //
